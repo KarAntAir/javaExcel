a9a6fb1364ac7418943af25286ee4670ba77d75c
Alexander Komarov <evilantik@gmail.com>
2023-06-15 23:04:25 +0200

    docs4j

--- src/main/java/JavaExcel.java
+++ src/main/java/JavaExcel.java
@@ -1,3 +1,4 @@
+import com.ibm.icu.text.RuleBasedNumberFormat;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.Font;
@@ -25,7 +26,24 @@ public class JavaExcel {
     }
 
     public static void main(String[] args) {
-        new JavaExcel();
+//        String s1 = "ООО \"КОРОНА РУС\" Москва, ул. Каспийская, д. 22, корпус 1, стр. 5, этаж 5, пом. 9, к. 17А, офис 86 телефон: +7\u00A0(925)\u00A0641-85-57 р/с 40702810801840000325 АО \"АЛЬФА-БАНК\" к/с 30101810200000000593, БИК 044525593";
+//        String s2 = "ООО \"КОРОНА РУС\" Москва, ул. Каспийская, д. 22, корпус 1, стр. 5, этаж 5, пом. 9, к. 17А, офис 86 телефон: +7 (925) 641-85-57 р/с 40702810801840000325 АО \"АЛЬФА-БАНК\" к/с 30101810200000000593, БИК 044525593";
+//        System.out.println(s1.length());
+//        System.out.println(s2.length());
+//        String s = "ООО \"КОРОНА РУС\" Москва, ул. Каспийская, д. 22, корпус 1, стр. 5, этаж 5, пом. 9, к. 17А, офис 86 телефон: +7 (925) 641-85-57 р/с 40702810801840000325 АО \"АЛЬФА-БАНК\" к/с 30101810200000000593, БИК 044525593ООО \"КОРОНА РУС\" Москва, ул. Каспийская, д. 22, корпус 1, стр. 5, этаж 5, пом. 9, к. 17А, офис 86 телефон: +7 (925) 641-85-57 р/с 40702810801840000325 АО \"АЛЬФА-БАНК\" к/с 30101810200000000593, БИК 044525593";
+//        new JavaExcel();
+//        System.out.println(s);
+//        System.out.println(s1);
+
+        RuleBasedNumberFormat nf = new RuleBasedNumberFormat(Locale.forLanguageTag("ru"),
+                RuleBasedNumberFormat.SPELLOUT);
+        System.out.println(nf.format(1234567.0));
+//        try {
+//            PDFConverter.saveAsPdf2();
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     public static void doWork() {
@@ -46,7 +64,7 @@ public class JavaExcel {
         @Override
         protected Object doInBackground() {
             int startIndex = 3;
-            double endIndex = ExcelCellNumbers.rowsDataList.size();
+            double endIndex = 5;//ExcelCellNumbers.rowsDataList.size();
             double progressPart = 100d / endIndex;
             double currentProgress = 0d;
             String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(Calendar.getInstance().getTime());
@@ -103,6 +121,33 @@ public class JavaExcel {
                     FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
                     formulaEvaluator.evaluateAll();
 
+                    workbook.setPrintArea(
+                            0, //sheet num
+                            0, //start column
+                            186, //end column
+                            0, //start row
+                            44 //end row
+                    );
+                    workbook.setPrintArea(
+                            1, //sheet num
+                            0, //start column
+                            189, //end column
+                            0, //start row
+                            60 //end row
+                    );
+
+                    sheet1.getPrintSetup().setPaperSize(PrintSetup.A4_PAPERSIZE);
+                    sheet2.getPrintSetup().setPaperSize(PrintSetup.A4_PAPERSIZE);
+                    sheet1.setFitToPage(true);
+                    sheet2.setFitToPage(true);
+                    sheet1.getPrintSetup().setFitWidth((short)1);
+                    sheet1.getPrintSetup().setFitHeight((short)1);
+                    sheet2.getPrintSetup().setFitWidth((short)1);
+                    sheet2.getPrintSetup().setFitHeight((short)1);
+
+
+
+
                     inputStream.close();
 
                     FileOutputStream os = new FileOutputStream(fileTTNRes);
@@ -111,6 +156,9 @@ public class JavaExcel {
                     os.close();
                     fillLogFile(fileNameTTNPart, i);
                     currentProgress += progressPart;
+
+
+
 
                     UI.progressBar.setValue(new Double(currentProgress).intValue());
                 }
@@ -172,6 +220,17 @@ public class JavaExcel {
                         errors.put("Ошибка замены", "нет данных для замены");
                         resCell.setCellValue("ОШИБКА");
                     } else {
+//                        String cellId = mapping.getColumn() + (mapping.getRow() + 1);
+//                        if (cellId.equals("N4")) {
+//                            Font font = resCell.getSheet().getWorkbook().createFont();
+//                            font.setFontHeight((short)100);
+//                            resCell.getCellStyle().setFont(font);
+//                            resCell.getCellStyle().setWrapText(true);
+//                        }
+
+//                        resCell.getCellStyle().setShrinkToFit(true);
+//                        resCell.getRow().setHeight((short) -1);
+
                         resCell.setCellValue(replaceResult);
                     }
                 } else {
